69958d1f590de6875b5255ee4fd86da9fcab3e8a
gokay aydogan <gokaygokay@users.noreply.huggingface.co>
2024-06-19 09:33:10 +0000

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ import subprocess
 subprocess.run('pip install flash-attn --no-build-isolation', env={'FLASH_ATTENTION_SKIP_CUDA_BUILD': "TRUE"}, shell=True)
 
 model_id = 'microsoft/Florence-2-large'
-model = AutoModelForCausalLM.from_pretrained(model_id, trust_remote_code=True).eval()
+model = AutoModelForCausalLM.from_pretrained(model_id, trust_remote_code=True).to("cuda").eval()
 processor = AutoProcessor.from_pretrained(model_id, trust_remote_code=True)
 
 
@@ -28,13 +28,14 @@ def fig_to_pil(fig):
     fig.savefig(buf, format='png')
     buf.seek(0)
     return Image.open(buf)
-    
+
+@spaces.GPU
 def run_example(task_prompt, image, text_input=None):
     if text_input is None:
         prompt = task_prompt
     else:
         prompt = task_prompt + text_input
-    inputs = processor(text=prompt, images=image, return_tensors="pt")
+    inputs = processor(text=prompt, images=image, return_tensors="pt").to("cuda")
     generated_ids = model.generate(
         input_ids=inputs["input_ids"],
         pixel_values=inputs["pixel_values"],
